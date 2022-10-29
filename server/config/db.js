d333f3ba0cfa122fa2682340d8650f316d5a5ea6
wwong174@my.centennialcollege.ca <wwong174@my.centennialcollege.ca>
2022-10-28 22:42:47 -0400

    Connect project to my MongoDB host

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 //db.js Wai Tung wong 301222578 2022-10-26  Mid-Term Test
 module.exports = {
   //local MongoDB deployment ->
-  "URI":"mongodb://127.0.0.1:27017/books229"
+  //"URI":"mongodb://127.0.0.1:27017/books229"
+
+  //MongoDB server for the midterm user password: pqkz3eNIUudfgdeu
+  "URI":"mongodb+srv://WWT:pqkz3eNIUudfgdeu@mongodbserver.lrapknz.mongodb.net/midtermBooks?retryWrites=true&w=majority"
 };
